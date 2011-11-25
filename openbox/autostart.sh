b70333af0f54c8ec00986fa212cb58f515766026
Marcos López <feirlane@gmail.com>
2011-11-25 17:36:17 +0100

    commit ALL THE CONFIGS

---
+++ openbox/autostart.sh
@@ -0,0 +1,6 @@
+# This file goes in $HOME/.config/openbox/autostart.sh
+
+tint2 &
+setxkbmap es
+synclient VertEdgeScroll=1
+Esetroot -c /home/feirlane/Dropbox/gameshirt_starcraft_2_zerg_vintage_logo_t_shirt.jpg
