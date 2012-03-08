90a875953bc1880bd925bfcc2e2ca1b38e563340
Marcos López <feirlane@gmail.com>
2012-03-08 16:15:15 +0100

    theme change

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -3,5 +3,5 @@
 tint2 &
 setxkbmap es
 synclient VertEdgeScroll=1
-Esetroot -c /home/feirlane/Dropbox/gameshirt_starcraft_2_zerg_vintage_logo_t_shirt.jpg
+Esetroot -c /home/feirlane/.bg.png
 dropboxd &
